289176b621db1f9b05f0d88b120074e80c07b09c
Aaron J Elmore <aelmore@tale.cs.ucsb.edu>
2014-06-03 20:47:48 -0700

    scripts

--- runmany.sh
+++ runmany.sh
@@ -2,14 +2,14 @@
 
 cmd='./oltpbenchmark --b ycsb -c config/pg_ycsb_config.xml --create=true --load=true --execute=true '
 
-RUNS=( 1 10 100 1000 10000)
+RUNS=( 1 10 100 1000 5000 10000 20000 50000 100000 200000 400000 1000000)
 
 
 echo "running"
 for i in "${RUNS[@]}"
 do
-   runcmd="$cmd -nt $i -o nt$i"
+   runcmd="$cmd -nt $i -o nt$i "
    echo $runcmd
-   $runcmd
+   $runcmd &> nt$i.log
 done
 
